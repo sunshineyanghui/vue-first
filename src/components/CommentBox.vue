f21f194178b8cb986f4fdfce7c2e663d202bcd6c
yanghui <yanghui@daishu360.com>
2017-09-13 11:31:09 +0800

    commentbox

---
+++ src/components/CommentBox.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <div class="comment-box">CommentBox</div>
+</template>
+
+<script>
+export default {
+  name:'comment-box'
+}
+</script>
+
+<style lang="css">
+</style>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,15 +1,18 @@
 <template lang="html">
   <div class="post">
     <post-body>postbody</post-body>
+    <comment-box>commentbox</comment-box>
   </div>
 </template>
 
 <script>
 import PostBody from './PostBody'
+import CommentBox from './CommentBox'
 export default {
   name:'Post',
   components:{
-    PostBody
+    PostBody,
+    CommentBox
   }
 }
 </script>
